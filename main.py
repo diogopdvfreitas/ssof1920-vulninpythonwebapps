af398e35bd6f7bf2b27bf907d1ed1fbf7dcd048d
asgcfigueiredo <alexandra.figueiredo@tecnico.ulisboa.pt>
2019-11-18 01:37:33 +0000

    Read files

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+import json
+import os
+from vulnerability import Vuln
+
+'''Read patterns file, creates object with information about vulnerabilities (sources, sanitization methods and sinks) 
+and stores in a list. '''
+def read_patterns(pattern_file):
+    if os.path.exists(pattern_file):
+        with open(pattern_file) as json_file:
+            patterns = json.load(json_file)
+    else:
+        print("Patterns file: " + pattern_file + " doesn't exist")
+        sys.exit(1)
+
+    vulns = []
+    for pattern in patterns:
+        vuln_name = pattern['vulnerability']
+        vuln_exists = False
+        for vuln in vulns:
+            if(vuln_name == vuln.vulnerability):
+                vuln.add_sources(pattern['sources'])
+                vuln_exists = True
+                break
+        if(not vuln_exists):    
+            vuln_obj = Vuln(pattern['vulnerability'], pattern['sources'], pattern['sanitizers'], pattern['sinks'])
+            vulns.append(vuln_obj)
+    return vulns
+
+
+def read_program(program_file):
+    if os.path.exists(program_file):
+        with open(program_file, encoding='utf-8') as json_file:
+            program = json.load(json_file)
+    else:
+        print("Patterns file: " + program_file + " doesn't exist")
+        sys.exit(1)
+    return program['body']
+
+
+'''Main'''
+if(len(sys.argv) != 3):
+    print("Please provide the program to be analyzed and the patterns")
+    exit(0)
+
+pattern_file = sys.argv[2]
+vulns = read_patterns(pattern_file)
+
+program_file = sys.argv[1]
+program = read_program(program_file)
+
+
+
+
+
+
+
+
